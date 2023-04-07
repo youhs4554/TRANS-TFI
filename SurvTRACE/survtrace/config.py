da22227e3601b49e605bb6bae82419e0d2d7e1d9
hossay-work <youhs4554@gmail.com>
2023-04-07 17:51:23 +0900

    Experiment v1.1:longer sequence

--- SurvTRACE/survtrace/config.py
+++ SurvTRACE/survtrace/config.py
@@ -7,7 +7,7 @@ STConfig = EasyDict(
         'custom_training': True, # Run custom training code if True, run original SurvTrace training code
         'data': 'metabric', # dataset name, in 'metabric', 'support', or 'seer'
         'num_durations': 5, # num of discrete intervals for prediction, e.g., num_dur = 5 means the whole period is discretized to be 5 intervals
-        'horizons': [.25, .5, .75], # the discrete intervals are cut at 0%, 25%, 50%, 75%, 100%
+        'horizons': [round(i, 2)/100 for i in range(5, 96, 5)], # the discrete intervals are cut at 5%, 10%, 15%, ..., 95%
         'seed': 1234,
         'checkpoint': Path('./model_dir'),
         'vocab_size': 8, # num of all possible values of categorical features
